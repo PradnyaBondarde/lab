65043cb0c35b193d012aca44bc73f860ff064bea
PradnyaBondarde <pradnya.bondarde@gmail.com>
2024-03-04 07:42:41 +0530

    main1 class of TMS

---
+++ main1.java
@@ -0,0 +1,19 @@
+//the first part of main class
+public class Main {
+public static void main(String[] args){
+TaskManager taskManager = new TaskManager();
+Scanner scanner=new Scanner(System.in);
+while(true){
+System.out.println("1.Add task");
+System.out.println("2.Edit task");
+System.out.println("3.Delete task");
+System.out.println("4.Display tasks");
+System.out.println("5.Sort tasks by progess");
+System.out.println("6.Exit");
+System.out.println("Enter your choice : ");
+int choice = scanner.nextInt();
+scanner.nextLine(); 
+//Switch statement
+}
+}
+}
